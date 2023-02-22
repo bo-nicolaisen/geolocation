011957d04379c77dab58be80063add15e39da3c7
bo-nicolaisen <31656337+bo-nicolaisen@users.noreply.github.com>
2023-02-22 13:57:21 +0100

    geo location and reverse geocoding by 2 apis

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,8 +2,7 @@
 
 const myApp = document.getElementById('app');
 
-
-const options = {
+const geoOptions = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
@@ -11,13 +10,8 @@ const options = {
 
 function success(pos) {
     const crd = pos.coords;
-
-    let myText = `${crd} Your current position is: Latitude : ${crd.latitude} Longitude: ${crd.longitude} More or less ${crd.accuracy} meters.`;
-
-    myApp.innerText = myText;
-
+    //myApp.innerText = `${crd} Your current position is: Latitude : ${crd.latitude} Longitude: ${crd.longitude} More or less ${crd.accuracy} meters.`;
     getLocationName(crd.longitude, crd.latitude);
-
 }
 
 function error(err) {
@@ -26,30 +20,49 @@ function error(err) {
 
 
 //myApp.innerText = 'test';
-navigator.geolocation.getCurrentPosition(success, error, options);
+navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+
 
 function getLocationName(myLong, myLat) {
-    const myApiKey = '3e88f8919e892cfdc1b365a34e48b092';
-    const apiUrl = `http://api.positionstack.com/v1/reverse?access_key=${myApiKey}&query=${myLat},${myLong}`;
 
+
+
+    // geo code api https://locationiq.com
+    //const myApiKey = 'pk.439b01bdf321f632f87bd4d2631fe6d0';
+    //const apiUrl = `https://eu1.locationiq.com/v1/reverse?key=${myApiKey}&lat=${myLat}&lon=${myLong}&format=json`;
+
+
+
+// geo code api https://nominatim  no token
+
+//url https://nominatim.openstreetmap.org/reverse?lat=56.94&lon=10.06&format=json
+const apiUrl = `https://nominatim.openstreetmap.org/reverse?lat=${myLat}&lon=${myLong}&format=json`;
+
+console.log(apiUrl);
 
     let myResElement = document.createElement('h2');
 
+ let fetchOptions={
 
-    fetch(apiUrl, {
-        Method: 'GET',
-        Headers: {
-            Accept: 'application.json',
-            'Content-Type': 'application/json'
-        },
-        Body: body,
-        Cache: 'default'
-    })
+    Method: 'GET',
+    Body: 'body',
+    Cache: 'default',
+
+    Headers: {
+        'Accept': 'application.json',
+        'Content-Type': 'application/json'
+    }
+
+
+ };
+
+    fetch(apiUrl,fetchOptions)
         .then((response) => response.json())
 
         .then((data) => {
-            console.log('Success:', data);
-            myResElement.innerText = `my data: ${data}`;
+            console.log('my fetched data:', data);
+
+            myResElement.innerText = `Du er i  ${data.address.hamlet} - ${data.address.postcode} ${data.address.village} `;
         })
         .catch((error) => {
             console.error('Error:', error);
