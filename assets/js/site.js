5ad620b113bafe74876b90c2a72674380e943066
bo-nicolaisen <31656337+bo-nicolaisen@users.noreply.github.com>
2023-04-11 12:16:47 +0200

    removed api key api

--- assets/js/site.js
+++ assets/js/site.js
@@ -8,35 +8,30 @@ const geoOptions = {
     maximumAge: 0
 };
 
+navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+
+
+
 function success(pos) {
     const crd = pos.coords;
-    //myApp.innerText = `${crd} Your current position is: Latitude : ${crd.latitude} Longitude: ${crd.longitude} More or less ${crd.accuracy} meters.`;
+
+   console.log( `${crd} Your current position is: Latitude : ${crd.latitude} Longitude: ${crd.longitude} More or less ${crd.accuracy} meters.`);
     getLocationName(crd.longitude, crd.latitude);
 }
+
 
 function error(err) {
     myApp.innerText = `ERROR(${err.code}): ${err.message}`;
 }
 
 
-//myApp.innerText = 'test';
-navigator.geolocation.getCurrentPosition(success, error, geoOptions);
-
 
 function getLocationName(myLong, myLat) {
 
-
-
-    // geo code api https://locationiq.com
-    //const myApiKey = 'pk.439b01bdf321f632f87bd4d2631fe6d0';
-    //const apiUrl = `https://eu1.locationiq.com/v1/reverse?key=${myApiKey}&lat=${myLat}&lon=${myLong}&format=json`;
-
-
-
-// geo code api https://nominatim  no token
-
-//url https://nominatim.openstreetmap.org/reverse?lat=56.94&lon=10.06&format=json
+// geo code api https://nominatim  no api key  use this
+console.log(myLong, myLat);
 const apiUrl = `https://nominatim.openstreetmap.org/reverse?lat=${myLat}&lon=${myLong}&format=json`;
+
 
 console.log(apiUrl);
 
@@ -55,14 +50,15 @@ console.log(apiUrl);
 
 
  };
+ // fetch(apiUrl,fetchOptions)
 
-    fetch(apiUrl,fetchOptions)
+    fetch(apiUrl)
         .then((response) => response.json())
 
         .then((data) => {
             console.log('my fetched data:', data);
 
-            myResElement.innerText = `Du er i  ${data.address.hamlet} - ${data.address.postcode} ${data.address.village} `;
+            myResElement.innerText = `Du er i  ${data.address.municipality} - ${data.address.postcode} ${data.address.city} `;
         })
         .catch((error) => {
             console.error('Error:', error);
